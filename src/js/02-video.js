df1efef7e5224a327bfbf6b5e4d28666d6a4fc15
Yehorborovenskyi <yehorborovenskyi@gmail.com>
2023-02-19 14:25:07 +0300

    create task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(currentTime, 1000));
+function currentTime(data) {
+  localStorage.setItem('videoplayer-time', data.seconds);
+}
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-time'))
+  .catch(function (error) {
+    console.error(error.name);
+    console.log(error.message);
+  });
